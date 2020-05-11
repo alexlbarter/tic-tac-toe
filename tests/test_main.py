044420782d9ba8de90925c2eb5496c01c96d54d0
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-05-11 18:24:44 +0100

    updated to use subTest for test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,16 +8,18 @@ class TestMain(unittest.TestCase):
         cases = (("1", 0), ("2", 1), ("3", 2), ("4", 3), ("5", 4), ("6", 5), ("7", 6), ("8", 7), ("9", 8))
         game = main.Game(1)
         for case in cases:
-            with unittest.mock.patch("builtins.input", return_value=case[0]):
-                self.assertEqual(game.get_move(), case[1])
+            with self.subTest(case=case):
+                with unittest.mock.patch("builtins.input", return_value=case[0]):
+                    self.assertEqual(game.get_move(), case[1])
 
     def test_get_move_invalid_numbers(self):
         cases = ("0", "10", "0.5")
-        with self.assertRaises(ValueError):
-            game = main.Game(1)
-            for case in cases:
+        game = main.Game(1)
+        for case in cases:
+            with self.subTest(case=case):
                 with unittest.mock.patch("builtins.input", return_value=case):
-                    game.get_move()
+                    with self.assertRaises(ValueError):
+                        game.get_move()
 
     def test_get_move_valid_words(self):
         cases = (("top left", 0), ("top middle", 1), ("top right", 2),
@@ -25,16 +27,18 @@ class TestMain(unittest.TestCase):
                  ("bottom left", 6), ("bottom middle", 7), ("bottom right", 8))
         game = main.Game(1)
         for case in cases:
-            with unittest.mock.patch("builtins.input", return_value=case[0]):
-                self.assertEqual(game.get_move(), case[1])
+            with self.subTest(case=case):
+                with unittest.mock.patch("builtins.input", return_value=case[0]):
+                    self.assertEqual(game.get_move(), case[1])
 
     def test_get_move_invalid_words(self):
         cases = ("top", "bottom", "left", "right")
-        with self.assertRaises(ValueError):
-            game = main.Game(1)
-            for case in cases:
+        game = main.Game(1)
+        for case in cases:
+            with self.subTest(case=case):
                 with unittest.mock.patch("builtins.input", return_value=case):
-                    game.get_move()
+                    with self.assertRaises(ValueError):
+                        game.get_move()
 
 
 if __name__ == '__main__':
