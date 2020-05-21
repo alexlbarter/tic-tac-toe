560f4a3bc22e0dab1ea1e942402d62b37d34883c
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-05-21 19:09:16 +0100

    split play_game into separate functions

--- scripts/discord_bot.py
+++ scripts/discord_bot.py
@@ -16,27 +16,36 @@ async def on_ready():
     print(f"Bot {bot.user} has connected")
 
 
-@bot.command(name="play")
-async def play_game(ctx, action, *move):
-    if action == "new":
+@bot.command(name="create")
+async def create_game(ctx):
+    for game in bot.current_games:
+        if game.username == ctx.author:
+            await ctx.send("You are already playing a game")
+            break
+    else:
+        game = main.Game(1, mode="discord", username=ctx.author)
+        bot.current_games.append(game)
+        await ctx.send("New game created")
+
+
+@bot.command(name="move")
+async def make_move(ctx, *move):
+    for game in bot.current_games:
+        if game.username == ctx.author:
+            break
+    else:
+        game = main.Game(1, mode="discord", username=ctx.author)
+        bot.current_games.append(game)
+        await ctx.send("New game created")
+
+    await ctx.send(f"Interpreted move:\n```{' '.join(move)}```")
+
+
+@bot.command(name="query")
+async def query(ctx, action):
+    if action == "games":
+        await ctx.send(f"There are currently {len(bot.current_games)} people playing")
         for game in bot.current_games:
-            if game.username == ctx.author:
-                await ctx.send("You are already playing a game")
-                break
-        else:
-            game = main.Game(1, mode="discord", username=ctx.author)
-            bot.current_games.append(game)
-            await ctx.send("New game created")
-    elif action == "move":
-        for game in bot.current_games:
-            if game.username == ctx.author:
-                break
-        else:
-            game = main.Game(1, mode="discord", username=ctx.author)
-            bot.current_games.append(game)
-            await ctx.send("New game created")
-
-
-
+            await ctx.send(game.username)
 
 bot.run(BOT_TOKEN)
